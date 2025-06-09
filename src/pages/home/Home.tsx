7c7238dadc9ba4988546f68b1418425af1331ae2
DoDuy2004 <135083040+DoDuy2004@users.noreply.github.com>
2025-06-10 01:05:55 +0700

    Add Home/HeroSection

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import HeroSection from "./components/HeroSection";
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  return (
+    <div
+      className="bg-cover bg-no-repeat w-full"
+      style={{ backgroundImage: `url(/assets/images/BG.1.svg)` }}
+    >
+        <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
